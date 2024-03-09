60b86a594ef1754a8fb61aecab3562011508f212
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2024-03-08 21:34:48 -0300

    fix: ajustes en las cards para que no se genere overflow

--- alura-cinema/src/components/Card/index.js
+++ alura-cinema/src/components/Card/index.js
@@ -3,21 +3,23 @@ import styles from "./Card.module.css";
 import iconFavorito from "./iconFavorito.png";
 import iconNoFavorito from "./iconNoFavorito.png";
 function Card({ id, capa, titulo }) {
-  
-  const {favorito , agregarFavorito}= useFavoritosContext()
-  const isFavorito = favorito.some(fav=> fav.id === id)
-  const icon = isFavorito ? iconFavorito : iconNoFavorito 
+
+  const { favorito, agregarFavorito } = useFavoritosContext()
+  const isFavorito = favorito.some(fav => fav.id === id)
+  const icon = isFavorito ? iconFavorito : iconNoFavorito
 
   return (
     <div className={styles.container}>
       <img src={capa} alt={titulo} className={styles.capa} />
-      <h2>{titulo}</h2>
-      <img
-        src={icon}
-        alt="Icono favorito"
-        className={styles.favorito}
-        onClick={()=> agregarFavorito({id, titulo, capa})}
-      />
+      <div className={styles.cardBody}>
+        <h2>{titulo}</h2>
+        <img
+          src={icon}
+          alt="Icono favorito"
+          className={styles.favorito}
+          onClick={() => agregarFavorito({ id, titulo, capa })}
+        />
+      </div>
     </div>
   );
 }
