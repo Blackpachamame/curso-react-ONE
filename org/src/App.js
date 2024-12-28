41e2ce043ab9d6d58e302aaa23fc09d0e2621e49
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2024-12-27 23:03:29 -0300

    fix colaborador spinmari

--- org/src/App.js
+++ org/src/App.js
@@ -1,104 +1,108 @@
-import { useState, useEffect } from 'react';
-import { v4 as uuid } from 'uuid';
-import Header from './components/Header/Header';
-import Formulario from './components/Formulario/Formulario';
-import MiOrg from './components/MiOrg';
-import Equipo from './components/Equipo';
-import Footer from './components/Footer';
+import { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import Header from "./components/Header/Header";
+import Formulario from "./components/Formulario/Formulario";
+import MiOrg from "./components/MiOrg";
+import Equipo from "./components/Equipo";
+import Footer from "./components/Footer";
 
 function App() {
   const [mostrarForm, actualizarMostrar] = useState(false);
 
-  const colaboradoresArreglo = [{
-    id: uuid(),
-    equipo: "Instructor",
-    foto: "https://github.com/harlandlohora.png",
-    nombre: "Harland Lohora",
-    puesto: "Instructor e Ing. de Software",
-    fav: true,
-  },
-  {
-    id: uuid(),
-    equipo: "Backend",
-    foto: "https://github.com/genesysR-dev.png",
-    nombre: "Genesys Rondón",
-    puesto: "Desarrolladora de Software",
-    fav: false,
-  },
-  {
-    id: uuid(),
-    equipo: "Devops",
-    foto: "https://github.com/JeanmarieAluraLatam.png",
-    nombre: "Jeanmarie Quijada",
-    puesto: "Instructora e Ing. en Sistemas",
-    fav: true,
-  },
-  {
-    id: uuid(),
-    equipo: "Innovación y Gestión",
-    foto: "https://github.com/christianpva.png",
-    nombre: "Christian Velasco",
-    puesto: "Head de Alura e Instructor",
-    fav: false,
-  },
-  {
-    id: uuid(),
-    equipo: "Frontend",
-    foto: "https://github.com/JoseDarioGonzalezCha.png",
-    nombre: "Jose Gonzalez",
-    puesto: "Desarrollador Full Stack",
-    fav: true,
-  },
-  {
-    id: uuid(),
-    equipo: "Innovación y Gestión",
-    foto: "https://github.com/RaquelCoimbra.png",
-    nombre: "Raquel Coimbra",
-    puesto: "Community Manager",
-    fav: true,
-  },
-  {
-    id: uuid(),
-    equipo: "Innovación y Gestión",
-    foto: "https://github.com/mauricioalura.png",
-    nombre: "Maurício Santiago",
-    puesto: "Community Manager",
-    fav: false,
-  },
-  {
-    id: uuid(),
-    equipo: "Data Science",
-    foto: "https://github.com/estebanvz.png",
-    nombre: "Esteban Vilca",
-    puesto: "Instructor y Asesor técnico de inteligencia artificial",
-    fav: false,
-  },
-  {
-    id: uuid(),
-    equipo: "Data Science",
-    foto: "https://github.com/ElProfeAlejo.png",
-    nombre: "Alejandro Gamarra",
-    puesto: "Instructor y Especialista en Big Data",
-    fav: false,
-  },
-  {
-    id: uuid(),
-    equipo: "Instructor",
-    foto: "https://github.com/Ellen-code.png",
-    nombre: "Ellen Pimentel",
-    puesto: "Instructora y Desarrolladora Full Stack",
-    fav: false,
-  },
-  {
-    id: uuid(),
-    equipo: "UX y Diseño",
-    foto: "https://github.com/Mapinko.png",
-    nombre: "Mariana Spinola",
-    puesto: "Diseñadora web y Desarrolladora Full Stack",
-    fav: true,
-  }];
+  const colaboradoresArreglo = [
+    {
+      id: uuid(),
+      equipo: "Instructor",
+      foto: "https://github.com/harlandlohora.png",
+      nombre: "Harland Lohora",
+      puesto: "Instructor e Ing. de Software",
+      fav: true,
+    },
+    {
+      id: uuid(),
+      equipo: "Backend",
+      foto: "https://github.com/genesysR-dev.png",
+      nombre: "Genesys Rondón",
+      puesto: "Desarrolladora de Software",
+      fav: false,
+    },
+    {
+      id: uuid(),
+      equipo: "Devops",
+      foto: "https://github.com/JeanmarieAluraLatam.png",
+      nombre: "Jeanmarie Quijada",
+      puesto: "Instructora e Ing. en Sistemas",
+      fav: true,
+    },
+    {
+      id: uuid(),
+      equipo: "Innovación y Gestión",
+      foto: "https://github.com/christianpva.png",
+      nombre: "Christian Velasco",
+      puesto: "Head de Alura e Instructor",
+      fav: false,
+    },
+    {
+      id: uuid(),
+      equipo: "Frontend",
+      foto: "https://github.com/JoseDarioGonzalezCha.png",
+      nombre: "Jose Gonzalez",
+      puesto: "Desarrollador Full Stack",
+      fav: true,
+    },
+    {
+      id: uuid(),
+      equipo: "Innovación y Gestión",
+      foto: "https://github.com/RaquelCoimbra.png",
+      nombre: "Raquel Coimbra",
+      puesto: "Community Manager",
+      fav: true,
+    },
+    {
+      id: uuid(),
+      equipo: "Innovación y Gestión",
+      foto: "https://github.com/mauricioalura.png",
+      nombre: "Maurício Santiago",
+      puesto: "Community Manager",
+      fav: false,
+    },
+    {
+      id: uuid(),
+      equipo: "Data Science",
+      foto: "https://github.com/estebanvz.png",
+      nombre: "Esteban Vilca",
+      puesto: "Instructor y Asesor técnico de inteligencia artificial",
+      fav: false,
+    },
+    {
+      id: uuid(),
+      equipo: "Data Science",
+      foto: "https://github.com/ElProfeAlejo.png",
+      nombre: "Alejandro Gamarra",
+      puesto: "Instructor y Especialista en Big Data",
+      fav: false,
+    },
+    {
+      id: uuid(),
+      equipo: "Instructor",
+      foto: "https://github.com/Ellen-code.png",
+      nombre: "Ellen Pimentel",
+      puesto: "Instructora y Desarrolladora Full Stack",
+      fav: false,
+    },
+    {
+      id: uuid(),
+      equipo: "UX y Diseño",
+      foto: "https://github.com/SpinMari.png",
+      nombre: "Mariana Spinola",
+      puesto: "Diseñadora web y Desarrolladora Full Stack",
+      fav: true,
+    },
+  ];
 
-  const [colaboradores, actualizarColaboradores] = useState(getInitialColaboradores);
+  const [colaboradores, actualizarColaboradores] = useState(
+    getInitialColaboradores
+  );
 
   const [equipos, actualizarEquipos] = useState([
     {
@@ -144,7 +148,9 @@ function App() {
 
   function getInitialColaboradores() {
     const savedColaboradores = localStorage.getItem("colaboradores");
-    return savedColaboradores ? JSON.parse(savedColaboradores) : colaboradoresArreglo;
+    return savedColaboradores
+      ? JSON.parse(savedColaboradores)
+      : colaboradoresArreglo;
   }
 
   const cambiarMostrar = () => {
@@ -159,7 +165,9 @@ function App() {
 
   //Eliminar colaborador
   const eliminarColaborador = (id) => {
-    const nuevosColaboradores = colaboradores.filter((colaborador) => colaborador.id !== id);
+    const nuevosColaboradores = colaboradores.filter(
+      (colaborador) => colaborador.id !== id
+    );
     actualizarColaboradores(nuevosColaboradores);
   };
 
@@ -196,26 +204,28 @@ function App() {
   return (
     <div>
       <Header />
-      {
-        mostrarForm && <Formulario
+      {mostrarForm && (
+        <Formulario
           equipos={equipos.map((equipo) => equipo.titulo)}
           registrarColaborador={registrarColaborador}
           crearEquipo={crearEquipo}
         />
-      }
+      )}
 
       <MiOrg cambiarMostrar={cambiarMostrar} />
 
-      {
-        equipos.map((equipo) => <Equipo
+      {equipos.map((equipo) => (
+        <Equipo
           datos={equipo}
           key={equipo.titulo}
-          colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.equipo === equipo.titulo
+          )}
           eliminarColaborador={eliminarColaborador}
           actualizarColor={actualizarColor}
           like={like}
-        />)
-      }
+        />
+      ))}
 
       <Footer />
     </div>
